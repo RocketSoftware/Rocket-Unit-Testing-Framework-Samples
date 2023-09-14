f9d992e646121977d2cf58f929532f81aa3b4c04
Stephen Gennard <68948052+StephenGennard@users.noreply.github.com>
2023-09-14 15:05:33 +0100

    update

--- courseware/Module1/demo3/bld.bat
+++ courseware/Module1/demo3/bld.bat
@@ -1,7 +1,7 @@
 del *.dll *.o *.idy *.int *report.txt TEST*.xml *.pdf *.trx tests.html tests.md
 cbllink -V -D -Otests CalcSalesComm.cbl MFUT_mytest.cbl MFUT_AlwaysBroken.cbl
-if errorlevel 1 goto theend
-mfurun -verbose -dc:ansi -jit:nodebug -report:junit -report:trx -report:markdown tests.dll
+@REM if errorlevel 1 goto theend
+@REM mfurun -verbose -dc:ansi -jit:nodebug -report:junit -report:trx -report:markdown tests.dll
 
-:theend
+@REM :theend
 
--- courseware/Module1/demo3/bld.sh
+++ courseware/Module1/demo3/bld.sh
@@ -1,5 +1,5 @@
 set -e
 rm -f *.so *.o *.idy *.int
 cob -zgtU -o tests.so -e "" CalcSalesComm.cbl MFUT_mytest.cbl MFUT_AlwaysBroken.cbl
-cobmfurun_t -verbose -dc:ansi -report:junit -report:trx -report:markdown tests.so
+# cobmfurun_t -verbose -dc:ansi -report:junit -report:trx -report:markdown tests.so
 
